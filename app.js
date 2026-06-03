b46e9035fa407aa3c915667e0374f44b0e16cd7f
Red_apolo <hectorgoyeneche316@gmail.com>
2026-06-02 22:10:34 -0300

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-/* Código JS para activar animaciones */
-window.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".anim-fadeIn, .anim-slide, .anim-bounce").forEach((element) => {
-    element.classList.add("play");
-  });
-});
